bcf88c397475518a39766dcca7320f6e55b66395
Petalousa <TerraPetalousa@gmail.com>
2022-09-25 12:05:43 -0400

    Add generation of dummy data.

--- command.py
+++ command.py
@@ -1,12 +1,51 @@
-
+from random import randint
+import json
 
 BAR_WIDTH = 30
+
+
+names = [
+    "Alpha",
+    "Bravo",
+    "Charlie",
+    "Delta",
+    "Echo",
+    "Foxtrot",
+    "Golf",
+    "Hotel",
+    "Lima",
+    "Mike",
+    "November",
+    "Oscar",
+    "Papa",
+    "Quebec",
+    "Sierra",
+    "Tango",
+    "Uniform",
+    "Victor",
+    "Whiskey",
+    "Xray",
+    "Yankee"
+]
+
+def make_junk_data(width, depth):
+    ret_dict = {}
+    for _ in range(randint(1, width)):
+        name = names[randint(0, len(names)-2)]
+        if depth > 1:
+            ret_dict[name] = make_junk_data(width, depth - 1)
+        else:
+            ret_dict[name] = []
+            for _ in range(randint(0, width)):
+                leaf_name = names[randint(0, len(names)-2)]
+                ret_dict[name].append(leaf_name)
+    return ret_dict
 
 class Commander:
     def __init__(self):
         self.exit_warning = None
+        self.json_data = make_junk_data(5, 3)
         pass
-
 
     # generic function to continually diplay a menu
     # display_menu should print options
@@ -22,14 +61,30 @@ class Commander:
         print('='*width)
 
 
+    # NAVIGATE MENU
+
+    # MAIN MENU
+    def generate_random(self):
+        print("generating new data...")
+        self.exit_warning = "You have unsaved changes"
+        self.json_data = make_junk_data(5, 3)
+        print("done!")
+
+    def print_json(self):
+        self.exit_warning = None
+        self.print_bar()
+        print(json.dumps(self.json_data, indent=4))
+
+    def navigate_json(self):
+        print("NAV")
+
     def display_main(self):
         self.print_bar()
         print("--MAIN MENU--")
-        print("1. ")
-        print("2. ")
-        print("3. ")
+        print("1. generate random data")
+        print("2. print data")
+        print("3. navigate")
         print("0. quit")
-
 
     def handle_main(self):
         _in = input(">")
@@ -47,14 +102,11 @@ class Commander:
                     return False
 
         elif _in == "1":
-            self.exit_warning = "You have unsaved changes"
-            print("unsaved changes.")
+            self.generate_random()
         elif _in == "2":
-            self.exit_warning = None
-            print("saved changes.")
-            pass
+            self.print_json()
         elif _in == "3":
-            pass
+            self.navigate_json()
 
         return True
 
